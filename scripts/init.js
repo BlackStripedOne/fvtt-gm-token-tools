cab0b09ce8fe6cbac7e6f4600dc76766fcdd9f58
BlackStripedOne <123573329+BlackStripedOne@users.noreply.github.com>
2023-03-08 15:18:11 +0000

    Quickfix for bug.

--- scripts/init.js
+++ scripts/init.js
@@ -44,7 +44,6 @@ Hooks.on('canvasReady', async () => {
       // If it's an X or Y change, assume the token is just moving
       if (Object.hasOwn(diff, 'y') || Object.hasOwn(diff, 'x')) return
       // TODO otherwise update token / hud
-      Logger.debug(updateToken)
     });
   })
 })
